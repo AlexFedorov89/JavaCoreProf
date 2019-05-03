68a51a1514f749ef4919f379e5088759ac085199
Alex <alex.fedorov1989@yandex.ru>
2019-05-03 21:46:05 +0300

    I did all ex. for lesson 1.

---
+++ src/Lesson1/Fruit.java
@@ -0,0 +1,13 @@
+package Lesson1;
+
+public abstract class Fruit {
+    private double weightOneFruit;
+
+    public Fruit(double weightOneFruit) {
+        this.weightOneFruit = weightOneFruit;
+    }
+
+    public double getWeightOneFruit() {
+        return weightOneFruit;
+    }
+}
